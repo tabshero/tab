67585d6d1a0d156d00d1fd0bc2c3a54d95aeac3f
thakurakhilbirsingh <32491981+tabshero@users.noreply.github.com>
2017-10-03 11:53:41 -0500

    Add files via upload

---
+++ table.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void  main()
+{   int i,k,n;
+    printf("Enter a number\n");
+    scanf("%d",&n);
+    printf("The table of %d is:\n",n);
+    for(i=1;i<=10;i++)
+    {
+      k = i*n;
+      printf("%d*%d=%d\n",n,i,k);
+    }
+
+}
